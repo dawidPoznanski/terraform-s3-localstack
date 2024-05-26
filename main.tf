d098c8aecd3b7e43af3427c311a85270fd3bdc5d
dawidPoznanski <dawid.poznanski@outlook.com>
2024-05-26 18:16:57 +0200

    working?

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-localstack-bucket"
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.bucket
   acl    = "private"
 }
 
 resource "aws_s3_bucket_object" "example" {
-  bucket = aws_s3_bucket.my_bucket.bucket
-  key    = "example.txt"
+  bucket  = aws_s3_bucket.my_bucket.bucket
+  key     = "example.txt"
   content = "This is an example file."
 }
--- provider.tf
+++ provider.tf
@@ -9,11 +9,11 @@ terraform {
 
 provider "aws" {
   region                      = "us-east-1"
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
+  access_key                  = "test"
+  secret_key                  = "test"
+  s3_force_path_style         = true
   skip_credentials_validation = true
   skip_requesting_account_id  = true
-
   endpoints {
     s3 = "http://localhost:4566"
   }
