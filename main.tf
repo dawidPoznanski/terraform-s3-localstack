8b99a8cb26dfaf4df8d4948b9a8739ea2a4aceda
dawidPoznanski <dawid.poznanski@outlook.com>
2024-05-26 18:20:53 +0200

    work

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_acl" "my_bucket_acl" {
   acl    = "private"
 }
 
-resource "aws_s3_bucket_object" "example" {
+resource "aws_s3_object" "example" {
   bucket  = aws_s3_bucket.my_bucket.bucket
   key     = "example.txt"
   content = "This is an example file."
