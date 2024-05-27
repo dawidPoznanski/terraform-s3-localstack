cdf79ba689f69acc0903d90eb46e0bfa797297c6
dawidPoznanski <dawid.poznanski@outlook.com>
2024-05-27 16:32:37 +0200

    test

--- provider.tf
+++ provider.tf
@@ -10,10 +10,10 @@ terraform {
 provider "aws" {
   access_key                  = "test"
   secret_key                  = "test"
-  region                      = "eu-west-2"
+  region                      = "us-west-1"
   s3_force_path_style         = true
   skip_credentials_validation = true
-  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
   endpoints {
     s3 = "http://localhost:4566"
   }
