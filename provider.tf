8424330e4a4efe66075d28bf3f0af68d1447ac5a
dawidPoznanski <dawid.poznanski@outlook.com>
2024-05-27 16:29:37 +0200

    change provider

--- provider.tf
+++ provider.tf
@@ -7,14 +7,14 @@ terraform {
   }
 }
 
-# provider "aws" {
-#   access_key                  = "mock_access_key"
-#   secret_key                  = "mock_secret_key"
-#   region                      = "us-east-1"
-#   s3_force_path_style         = true
-#   skip_credentials_validation = true
-#   skip_metadata_api_check     = true
-#   endpoints {
-#     s3 = "http://localhost:4566"
-#   }
-# }
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "eu-west-2"
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
